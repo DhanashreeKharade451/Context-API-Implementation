8b8bc9d62a47e8e2d88d26879cef0b3bf2b0eea7
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-02 01:29:33 -0500

    created ThemeContext

---
+++ src/context/AppProviders.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import { TodoProvider } from "./TodoContext";
+import { FilterProvider } from "./FilterContext";
+import { ThemeProvider } from "./ThemeContext";
+
+export const AppProvider = ({children}:{children: ReactNode}) => {
+    return(
+        
+    )
+}
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -8,4 +8,26 @@ interface ThemeContextType {
  
 const ThemeContext = createContext<ThemeContextType | null>(null);
 
-export const ThemeProvider = ({ children }: { children: ReactNode }) => {}
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+    const stored = localStorage.getItem("theme") as ThemeType;
+    const [theme, setTheme] = useState<ThemeType>(stored || "light");
+
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+    },[theme]);
+
+    const toggleTheme = () =>
+        setTheme((prev) => (prev === "light"? "dark" : "light"));
+
+    return(
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => {
+    const context =useContext(ThemeContext);
+    if(!context) throw new Error ("useTheme must be inside ThemeProvider");
+    return context;
+};
