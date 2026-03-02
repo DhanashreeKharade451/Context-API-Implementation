1bbf01f70fa256f58f3163a9e7385f621ff8dce2
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-02 03:45:30 -0500

    List Item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,40 +1,68 @@
 import { useState } from "react";
-import type{ Todo } from "../types";
+import type { Todo } from "../types";
 import { useTodos } from "../context/TodoContext";
 
 interface Props {
-    todo: Todo;
+  todo: Todo;
 }
 
-export const TodoItem = ({todo}: Props) => {
-        const { toggleTodo, deleteTodo, editTodo } = useTodos();
+export const TodoItem = ({ todo }: Props) => {
+  const { toggleTodo, deleteTodo, editTodo } = useTodos();
   const [isEditing, setIsEditing] = useState(false);
-  const [editText, setEditText] = useState(todo.text);
+  const [editText, setEditText] = useState(todo.text); 
 
-  return(
+  const handleEditSubmit = () => {
+    if (!editText.trim())  return;
+     editTodo(todo.id, editText);
+     setIsEditing(false);
+  };
+
+  return (
     <li className="flex items-center justify-between bg-gray-50 dark:bg-gray-700 p-3 rounded-lg shadow-sm">
-        <div className="flex items-center gap-3 flex-1">
-            <input
-             type="checkbox" 
-             checked = {todo.completed}
-             onChange={() => toggleTodo(todo.id)}
-             className="w-4 h-4 accent-blue-600"
-            />
-      
+      <div className="flex items-center gap-3 flex-1">
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => toggleTodo(todo.id)}
+          className="w-4 h-4 accent-blue-600"
+        />
 
-        {isEditing? (<input/>):(
-            <span>{todo.text}</span>
+        {isEditing ? (
+          <input
+            value={editText}
+            onChange={(e) => setEditText(e.target.value)}
+            onBlur={handleEditSubmit}
+            onKeyDown={(e) => e.key === "Enter" && handleEditSubmit()}
+            className="flex-1 px-2 py-1 rounded border"
+            autoFocus
+          />
+        ) : (
+          <span
+            className={`flex-1 ${
+              todo.completed ? "line-through text-gray-500" : ""
+            }`}
+          >
+            {todo.text}
+          </span>
         )}
-          </div>
+      </div>
 
-          <div>
-            <button>
-                ✏️
-            </button>
-            <button>
-                 🗑️
-            </button>
-          </div>
+      <div className="flex gap-2">
+        <button
+          onClick={() => setIsEditing(true)}
+          className="px-2 py-1 text-sm bg-yellow-400 
+                     rounded hover:bg-yellow-500"
+        >
+          ✏️
+        </button>
+        <button
+          onClick={() => deleteTodo(todo.id)}
+          className="px-2 py-1 text-sm bg-red-500 
+                     text-white rounded hover:bg-red-600"
+        >
+          🗑️
+        </button>
+      </div>
     </li>
   );
 };
